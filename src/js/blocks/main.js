40b30a25f9ac1a535ad1fcc7a61193365b060e5e
Ola <musicola@mail.ru>
2023-08-04 19:49:10 +0300

    slider service

--- src/js/blocks/main.js
+++ src/js/blocks/main.js
@@ -1,24 +1,19 @@
 function main(){
-    const btnNextReviewsRight = document.querySelector('.main__vector-right-reviews');
-    const btnNextReviewsLeft = document.querySelector('.main__vector-left-reviews');
-    const swiperSlideOne = document.querySelector('.swiper-slide-one');
-    const swiperSlideTwo = document.querySelector('.swiper-slide-two');
+ 
+    let swiper = new Swiper(".mySwiper", {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
 
-        btnNextReviewsRight.addEventListener('click', function(){
-            console.log(1);
-            swiperSlideOne.classList.add('main__hidden');
-            btnNextReviewsRight.classList.add('main__hidden'); 
-            btnNextReviewsLeft.classList.remove('main__hidden');
-            swiperSlideTwo.classList.remove('main__hidden');
-        });
-        btnNextReviewsLeft.addEventListener('click', function(){
-            console.log(1);
-            swiperSlideTwo.classList.add('main__hidden');
-            btnNextReviewsLeft.classList.add('main__hidden'); 
-            btnNextReviewsRight.classList.remove('main__hidden');
-            swiperSlideOne.classList.remove('main__hidden');
-        });
-        
+    let swiperService = new Swiper(".myswiperService ", {
+        navigation: {
+         nextEl: ".swiperService-button-next",
+         prevEl: ".swiperService-button-prev",
+       },
+      });
+    
 
    
 }
