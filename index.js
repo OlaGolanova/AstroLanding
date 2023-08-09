bc2ea2c2c56505138dbf66f851be7b1599324de9
Ola <musicola@mail.ru>
2023-08-09 14:15:41 +0300

    Updates

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ window.addEventListener('DOMContentLoaded', () => {
     animation();
     rotate();
     main();
+    popup();
     window.addEventListener('resize', () => {
         fix100vh();
         findHeight();
@@ -283,6 +284,35 @@ function main(){
     
   
    
+}
+
+function popup(){
+    const btnsMores = document.querySelectorAll('.more');
+    const moreWindows = document.querySelectorAll('.main__service');
+    const popups = document.querySelectorAll('.popup');
+    const btnsClosePopup = document.querySelectorAll('.popup__item_btn-close');
+
+
+    btnsMores .forEach(function(item,i){
+        item.addEventListener('click', function(){
+            // popup.classList.remove('hidden');
+            popups[i].style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+    moreWindows.forEach(function(item,i){
+        item.addEventListener('click', function(){
+            popups[i].style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    btnsClosePopup.forEach(function(item,i){
+        item.addEventListener('click', function(){
+            popups[i].style.display = 'none';
+            document.body.style.overflow = '';
+        });
+    });
 }
 
 //Анимация
---
+++ js/blocks/popup.js
@@ -0,0 +1,28 @@
+function popup(){
+    const btnsMores = document.querySelectorAll('.more');
+    const moreWindows = document.querySelectorAll('.main__service');
+    const popups = document.querySelectorAll('.popup');
+    const btnsClosePopup = document.querySelectorAll('.popup__item_btn-close');
+
+
+    btnsMores .forEach(function(item,i){
+        item.addEventListener('click', function(){
+            // popup.classList.remove('hidden');
+            popups[i].style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+    moreWindows.forEach(function(item,i){
+        item.addEventListener('click', function(){
+            popups[i].style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    btnsClosePopup.forEach(function(item,i){
+        item.addEventListener('click', function(){
+            popups[i].style.display = 'none';
+            document.body.style.overflow = '';
+        });
+    });
+}
