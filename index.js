d280cdc56eee2332de779bd67323e1ec9ad36ae1
Ola <musicola@mail.ru>
2023-08-09 17:52:40 +0300

    Updates

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
     popup();
     window.addEventListener('resize', () => {
         fix100vh();
-        findHeight();
+        // findHeight();
     })
 });
 function fix100vh() {
@@ -287,20 +287,52 @@ function main(){
 }
 
 function popup(){
-    const btnsMores = document.querySelectorAll('.more');
-    const moreWindows = document.querySelectorAll('.main__service');
+    const btnsMoresLaptop = document.querySelectorAll('.main__service-more-laptop');
+    const btnsMoresTablet = document.querySelectorAll('.main__service-more-tablet');
+    const btnsMoresMobile = document.querySelectorAll('.main__service-more-mobile');
+    const moreWindowsLaptop = document.querySelectorAll('.main__service-laptop');
+    const moreWindowsTablet = document.querySelectorAll('.main__service-tablet');
+    const moreWindowsMobile = document.querySelectorAll('.main__service-mobile');
+    
     const popups = document.querySelectorAll('.popup');
     const btnsClosePopup = document.querySelectorAll('.popup__item_btn-close');
 
 
-    btnsMores .forEach(function(item,i){
+    btnsMoresLaptop.forEach(function(item,i){
         item.addEventListener('click', function(){
             // popup.classList.remove('hidden');
             popups[i].style.display = 'block';
             document.body.style.overflow = 'hidden';
         });
     });
-    moreWindows.forEach(function(item,i){
+    btnsMoresTablet.forEach(function(item,i){
+        item.addEventListener('click', function(){
+            // popup.classList.remove('hidden');
+            popups[i].style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+    btnsMoresMobile.forEach(function(item,i){
+        item.addEventListener('click', function(){
+            // popup.classList.remove('hidden');
+            popups[i].style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+    moreWindowsLaptop.forEach(function(item,i){
+        item.addEventListener('click', function(){
+            popups[i].style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    moreWindowsTablet.forEach(function(item,i){
+        item.addEventListener('click', function(){
+            popups[i].style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+    moreWindowsMobile.forEach(function(item,i){
         item.addEventListener('click', function(){
             popups[i].style.display = 'block';
             document.body.style.overflow = 'hidden';
--- js/blocks/popup.js
+++ js/blocks/popup.js
@@ -1,18 +1,50 @@
 function popup(){
-    const btnsMores = document.querySelectorAll('.main__service-more');
-    const moreWindows = document.querySelectorAll('.main__service');
+    const btnsMoresLaptop = document.querySelectorAll('.main__service-more-laptop');
+    const btnsMoresTablet = document.querySelectorAll('.main__service-more-tablet');
+    const btnsMoresMobile = document.querySelectorAll('.main__service-more-mobile');
+    const moreWindowsLaptop = document.querySelectorAll('.main__service-laptop');
+    const moreWindowsTablet = document.querySelectorAll('.main__service-tablet');
+    const moreWindowsMobile = document.querySelectorAll('.main__service-mobile');
+    
     const popups = document.querySelectorAll('.popup');
     const btnsClosePopup = document.querySelectorAll('.popup__item_btn-close');
 
 
-    btnsMores .forEach(function(item,i){
+    btnsMoresLaptop.forEach(function(item,i){
         item.addEventListener('click', function(){
             // popup.classList.remove('hidden');
             popups[i].style.display = 'block';
             document.body.style.overflow = 'hidden';
         });
     });
-    moreWindows.forEach(function(item,i){
+    btnsMoresTablet.forEach(function(item,i){
+        item.addEventListener('click', function(){
+            // popup.classList.remove('hidden');
+            popups[i].style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+    btnsMoresMobile.forEach(function(item,i){
+        item.addEventListener('click', function(){
+            // popup.classList.remove('hidden');
+            popups[i].style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+    moreWindowsLaptop.forEach(function(item,i){
+        item.addEventListener('click', function(){
+            popups[i].style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    moreWindowsTablet.forEach(function(item,i){
+        item.addEventListener('click', function(){
+            popups[i].style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+    moreWindowsMobile.forEach(function(item,i){
         item.addEventListener('click', function(){
             popups[i].style.display = 'block';
             document.body.style.overflow = 'hidden';
