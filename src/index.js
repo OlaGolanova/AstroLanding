71c6d7456e55f849ffdb1bd10c9cb671482cd7bf
Ola <musicola@mail.ru>
2023-08-10 23:55:36 +0300

    modal

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ window.addEventListener('DOMContentLoaded', () => {
     rotate();
     main();
     popup();
+    reviews();
     window.addEventListener('resize', () => {
         fix100vh();
         // findHeight();
@@ -362,6 +363,22 @@ function popup(){
     });
 }
 
+function reviews(){
+    const btnMoreReviews = document.querySelectorAll('.main__reviews-more');
+    const mainReviewsWindows = document.querySelectorAll('.main__reviews-card');
+    
+    btnMoreReviews.forEach(function(item,i){
+        item.addEventListener('click', function(){
+            if(this.textContent === "Читать полностью"){
+                mainReviewsWindows[i].style.height = "auto";
+                this.textContent = "Скрыть"; 
+            } else if(this.textContent === "Скрыть"){
+                mainReviewsWindows[i].style.height = "318px";
+                this.textContent = "Читать полностью";   
+            }
+        });
+    });
+}
 //Анимация
 
 function rotate() {
